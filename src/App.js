06f86b02f7d91fb26dfdeba4f80ca0868842b31f
zilva149 <114057257+zilva149@users.noreply.github.com>
2022-12-02 15:11:58 +0200

    done project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,55 @@
+import { useState, useEffect } from "react";
+import Joke from "./components/Joke";
+
 export default function App() {
+  const [jokes, setJokes] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
+  const fetchUsers = async (url) => {
+    try {
+      const resp = await fetch(url);
+      if (resp.status >= 200 && resp.status <= 299) {
+        const data = await resp.json();
+        setIsLoading(false);
+        setJokes(data.jokes);
+      } else {
+        setIsLoading(false);
+        setIsError(true);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchUsers("https://v2.jokeapi.dev/joke/Programming?amount=10");
+  }, []);
+
+  if (isLoading) {
+    return (
+      <main className="container flex">
+        <h1>Loading...</h1>
+      </main>
+    );
+  }
+
+  if (isError) {
+    return (
+      <main className="container flex">
+        <h1>Error while fetching data...</h1>
+      </main>
+    );
+  }
+
   return (
-    <main className="App">
-      <h1>helo world</h1>
+    <main className="container flex flex-column">
+      <h1 className="title">jokes</h1>
+      <section className="jokes-container grid">
+        {jokes.map((joke, i) => {
+          return <Joke key={i} {...joke} />;
+        })}
+      </section>
     </main>
   );
 }
---
+++ src/components/Joke.js
@@ -0,0 +1,9 @@
+export default function Joke(data) {
+  const { type, joke, setup, delivery } = data;
+  return (
+    <article className="joke flex flex-column">
+      <h2 className="joke-title">{type === "single" ? joke : setup}</h2>
+      {type === "twopart" && <p className="joke-delivery">{delivery}</p>}
+    </article>
+  );
+}
